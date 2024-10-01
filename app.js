3bfb6d3811e47a152b6bfd0cae1152824b8c3cb9
Kyle Le <158103657+Kyle-Le-create@users.noreply.github.com>
2024-10-01 16:50:28 -0500

    Update on my project 13

--- app.js
+++ app.js
@@ -2,7 +2,6 @@ const express = require("express");
 const cors = require("cors");
 const mongoose = require("mongoose");
 const mainRouter = require("./routes/index");
-// const auth = require("./middlewares/auth");
 
 const app = express();
 const { PORT = 3001 } = process.env;
--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,8 @@
 const router = require("express").Router();
 
-const { getUsers, getUserById, createUser } = require("../controllers/users");
+const { getUsers, updateUser } = require("../controllers/users");
 
-router.get("/", getUsers);
-router.post("/", createUser);
-router.get("/:id", getUserById);
+router.get("/me", getUser);
+router.patch("/me", updateUser);
 
 module.exports = router;
