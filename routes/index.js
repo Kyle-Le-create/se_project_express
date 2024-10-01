779b4fcdab794e5a5327f142af43ece463ad9025
Kyle Le <158103657+Kyle-Le-create@users.noreply.github.com>
2024-10-01 15:54:25 -0500

    New update on my project 13

--- routes/index.js
+++ routes/index.js
@@ -1,10 +1,15 @@
 const router = require("express").Router();
-const clothingItem = require("./clothingItems");
-const user = require("./users");
+const userRouter = require("./utils/users");
+const itemRouter = require("./clothingItems");
+const { createUser, login } = require("../controllers/users");
 const { NOT_FOUND } = require("../utils/errors");
+const auth = require("../middlewares/auth");
 
 router.use("/items", clothingItem);
 router.use("/users", user);
+router.post("/signin", login);
+router.post("/signup", createUser);
+router.use(auth);
 
 router.use((req, res) =>
   res.status(NOT_FOUND).send({ message: "Requested resource not found" })
