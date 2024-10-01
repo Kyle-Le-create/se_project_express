ee7e781f491925018b2bf88c27ba464d2af301a1
Kyle Le <158103657+Kyle-Le-create@users.noreply.github.com>
2024-10-01 17:05:35 -0500

    Update on my project 13

--- routes/index.js
+++ routes/index.js
@@ -1,5 +1,5 @@
 const router = require("express").Router();
-const userRouter = require("./utils/users");
+const userRouter = require("./users");
 const itemRouter = require("./clothingItems");
 const { createUser, login } = require("../controllers/users");
 const { NOT_FOUND } = require("../utils/errors");
--- routes/users.js
+++ routes/users.js
@@ -1,6 +1,6 @@
 const router = require("express").Router();
 
-const { getUsers, updateUser } = require("../controllers/users");
+const { getUser, updateUser } = require("../controllers/users");
 
 router.get("/me", getUser);
 router.patch("/me", updateUser);
