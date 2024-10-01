4c383941c2a2f1c2672841093572a8ba6aea5b00
Kyle Le <158103657+Kyle-Le-create@users.noreply.github.com>
2024-10-01 17:34:30 -0500

    Update on my project 13

--- routes/users.js
+++ routes/users.js
@@ -1,8 +1,9 @@
 const router = require("express").Router();
+const auth = require("../middlewares/auth");
 
 const { getUser, updateUser } = require("../controllers/users");
 
-router.get("/me", getUser);
+router.get("/me", auth, getUser);
 router.patch("/me", updateUser);
 
 module.exports = router;
